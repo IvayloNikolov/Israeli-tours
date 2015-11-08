3f6b3a309e4f286f4931eee9b3c19343770c4672
IvayloNikolov <bloodydoom3@gmail.com>
2015-11-08 06:02:16 +0200

    changed pictures, added footer, flags, and some formatting of text

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-$('li').click( function()
+var bg_url = $('#header').css('background-image');
+bg_url= /url\(http:\/\/localhost:63342\/Israeli-tours\/images([0-9]+).jpg\)/g.exec(bg_url);
+bg_url = bg_url[1];
+
+$('#slider li').click(function()
 {
     $('#header').backstretch("images"+$(this).attr("id")+".jpg");
+    alert($(this).attr("id"));
+    var imageNumber = $(this).attr("id");
+    if(imageNumber==1)
+    {
+        $("#header #headings h2").html('Feel sacred now');
+    }
+    else
+    {
+        $("#header #headings h2").html('BOOK A TOUR NOW');
+    }
+    if(imageNumber==5)
+    {
+        $("#header #headings h1").html('Enjoy Bethlehem today');
+    }
+    else
+    {
+        $("#header #headings h1").html('ENJOY JERUSALEM TODAY');
+    }
+    if(imageNumber==2 || imageNumber==3 || imageNumber==5)
+    {
+        $('#headings').animate({paddingTop:'60px'}, 1000);
+    }
+    else
+    {
+        $('#headings').animate({paddingTop:'160px'}, 1000);
+    }
 });
+
+$(window).on("backstretch.after", function (e, instance, index) {
+    bg_url = $('#header').css('background-image');
+    console.log(bg_url);
+});
+
 $('#header').on('mouseenter', function()
 {
     $('#slider').animate({marginTop:'15px'}, 1000);
@@ -10,3 +46,4 @@ $('#header').on('mouseleave', function()
 {
     $('#slider').animate({marginTop:'340px'}, 1000);
 });
+
