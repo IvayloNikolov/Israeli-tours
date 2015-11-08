ee534f5871bc1fb82309a4cb247eee2a62e6ac46
IvayloNikolov <bloodydoom3@gmail.com>
2015-11-08 17:27:36 +0200

    black and white pictures and menu design

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var bg_url = $('#header').css('background-image');
-bg_url= /url\(http:\/\/localhost:63342\/Israeli-tours\/images([0-9]+).jpg\)/g.exec(bg_url);
+bg_url= /url\(http:\/\/ivaylonikolov.net\/Israeli-tours\/images([0-9]+).jpg\)/g.exec(bg_url);
 bg_url = bg_url[1];
+var counter = 0;
+$('#header').backstretch("images1.jpg");
+
+$(document).scroll(function(){
+    counter++
+    if(counter==2)
+    {
+        counter=0;
+        $('#menu').slideDown();
+    }
+});
 
 $('#slider li').click(function()
 {
     $('#header').backstretch("images"+$(this).attr("id")+".jpg");
-    alert($(this).attr("id"));
     var imageNumber = $(this).attr("id");
     if(imageNumber==1)
     {
@@ -15,13 +25,25 @@ $('#slider li').click(function()
     {
         $("#header #headings h2").html('BOOK A TOUR NOW');
     }
+    if(imageNumber==1)
+    {
+        $("#header #headings h1").html('Enjoy Israel today')
+    }
+    if(imageNumber==2)
+    {
+        $("#header #headings h1").html("Enjoy JERUSALEM today");
+    }
+    if(imageNumber==3)
+    {
+        $("#header #headings h1").html('Enjoy Haifa today')
+    }
+    if(imageNumber==4)
+    {
+        $("#header #headings h1").html('Enjoy Dead Sea today')
+    }
     if(imageNumber==5)
     {
-        $("#header #headings h1").html('Enjoy Bethlehem today');
-    }
-    else
-    {
-        $("#header #headings h1").html('ENJOY JERUSALEM TODAY');
+        $("#header #headings h1").html('Enjoy Caesarea today');
     }
     if(imageNumber==2 || imageNumber==3 || imageNumber==5)
     {
@@ -32,6 +54,10 @@ $('#slider li').click(function()
         $('#headings').animate({paddingTop:'160px'}, 1000);
     }
 });
+function hide()
+{
+    $('#menu').slideUp();
+}
 
 $(window).on("backstretch.after", function (e, instance, index) {
     bg_url = $('#header').css('background-image');
@@ -46,4 +72,3 @@ $('#header').on('mouseleave', function()
 {
     $('#slider').animate({marginTop:'340px'}, 1000);
 });
-
